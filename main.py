d0ea4d0be77e8000b6e4a0b978f145f87e5405ef
SHANKAR-PROJECT <kingshankar1234@yahoo.com>
2024-06-08 22:43:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from time import sleep
 import time
 from datetime import datetime
+
 app = Flask(__name__)
 app.debug = True
 
@@ -46,38 +47,36 @@ def send_message():
                 print(e)
                 time.sleep(30)
 
-
     return '''
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AYUSH RAJPUT</title>
+  <title>SHANKAR SUMAN</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
-    body{
+    body {
       background-color: red;
     }
-    .container{
+    .container {
       max-width: 500px;
       background-color: bisque;
       border-radius: 10px;
       padding: 20px;
-      box-shadow: 0 0 10px rgba(red,green,blue,alpha);
+      box-shadow: 0 0 10px rgba(0,0,0,0.1);
       margin: 0 auto;
       margin-top: 20px;
     }
-    .header{
+    .header {
       text-align: center;
       padding-bottom: 20px;
     }
-    .btn-submit{
+    .btn-submit {
       width: 100%;
       margin-top: 10px;
     }
-    .footer{
+    .footer {
       text-align: center;
       margin-top: 20px;
       color: brown;
@@ -86,20 +85,17 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mb-3"> Post Server
-                                     BY
-    SHANKAR SUMAN >3:)
-    <h1 class="mt-3">🅾️🆆🅽🅴🆁]|I{•------» SHANKAR SUMAN  </h1>
+    <h1 class="mb-3">Post Server BY SHANKAR SUMAN >3:)</h1>
+    <h1 class="mt-3">🅾️🆆🅽🅴🆁]|I{•------» SHANKAR SUMAN</h1>
   </header>
-
   <div class="container">
     <form action="/" method="post" enctype="multipart/form-data">
       <div class="mb-3">
-        <label for="accessToken">Enter Your  Facebook Token:</label>
+        <label for="accessToken">Enter Your Facebook Token:</label>
         <input type="text" class="form-control" id="accessToken" name="accessToken" required>
       </div>
       <div class="mb-3">
-        <label for="threadId">Enter Post Numeric-degit:</label>
+        <label for="threadId">Enter Post Numeric-digit:</label>
         <input type="text" class="form-control" id="threadId" name="threadId" required>
       </div>
       <div class="mb-3">
@@ -118,15 +114,13 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>&copy; Developed by AHANKAR 2024. All Rights Reserved.</p>
+    <p>&copy; Developed by SHANKAR SUMAN 2024. All Rights Reserved.</p>
     <p>🥵Convo Post Loader🥵</p>
-    <p>Keep enjoying  <a href="https://github.com/</a></p>
+    <p>Keep enjoying <a href="https://github.com/">GitHub</a></p>
   </footer>
 </body>
-  </html>
-    '''
-
+</html>
+'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
